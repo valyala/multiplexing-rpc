c3d10f9ac943bd498388ecf3f229acbdc329f0e8
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-10-22 15:07:41 +0000

    added mrpc_server

---
+++ include/mrpc/mrpc_server.h
@@ -0,0 +1,22 @@
+#ifndef MRPC_SERVER_PUBLIC
+#define MRPC_SERVER_PUBLIC
+
+#include "mrpc/mrpc_common.h"
+#include "mrpc/mrpc_interface.h"
+#include "ff/arch/ff_arch_net_addr.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct mrpc_server;
+
+MRPC_API struct mrpc_server *mrpc_server_create(struct mrpc_interface *service_interface, void *service_ctx, struct ff_arch_net_addr *listen_addr);
+
+MRPC_API void mrpc_server_delete(struct mrpc_server *server);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/private/mrpc_server.h
@@ -0,0 +1,15 @@
+#ifndef MRPC_SERVER_PRIVATE
+#define MRPC_SERVER_PRIVATE
+
+#include "mrpc/mrpc_server.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
