7c27901cb0311aec35e6f01846507e8d390a9b80
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-10-30 14:01:41 +0000

    added mrpc_char_array_param

---
+++ include/private/mrpc_char_array_param.h
@@ -0,0 +1,15 @@
+#ifndef MRPC_CHAR_ARRAY_PARAM_PRIVATE
+#define MRPC_CHAR_ARRAY_PARAM_PRIVATE
+
+#include "mrpc/mrpc_char_array_param.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
