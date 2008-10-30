b97ad4f533266ab82c275898e1b11060c176389b
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-10-30 13:59:19 +0000

    added mrpc_char_array_serialization

---
+++ include/private/mrpc_char_array_serialization.h
@@ -0,0 +1,30 @@
+#ifndef MRPC_CHAR_ARRAY_SERIALIZATION_PRIVATE
+#define MRPC_CHAR_ARRAY_SERIALIZATION_PRIVATE
+
+#include "private/mrpc_common.h"
+#include "private/mrpc_char_array.h"
+#include "ff/ff_stream.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Serializes the char_array into the stream.
+ * See maximum length of the char_array, which can be serialized, is in the mrcp_char_array_serialization.c file.
+ * Returns FF_SUCCESS on success, FF_FAILURE on error.
+ */
+enum ff_result mrpc_char_array_serialize(const struct mrpc_char_array *char_array, struct ff_stream *stream);
+
+/**
+ * Unserializes char_array from the stream into the newly allocated char_array.
+ * The caller should call the mrpc_char_array_dec_ref() on the char_array when it is no longer required.
+ * Returns FF_SUCCESS on success, FF_FAILURE on error.
+ */
+enum ff_result mrpc_char_array_unserialize(struct mrpc_char_array **char_array, struct ff_stream *stream);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/mrpc_char_array_serialization.c
@@ -0,0 +1,73 @@
+#include "private/mrpc_common.h"
+
+#include "private/mrpc_char_array_serialization.h"
+#include "private/mrpc_char_array.h"
+#include "private/mrpc_int_serialization.h"
+#include "ff/ff_stream.h"
+
+/* the maximum char_array size.
+ * This limit is security-related - it doesn't allow to send extremely long char arrays
+ * in order to trigger out of memory errors.
+ * The ((1 << 14) - 1) length was chosen, because it can be encoded into maximum two bytes
+ * by the mrpc_uint32_serialize() function
+ */
+#define MAX_CHAR_ARRAY_SIZE ((1 << 14) - 1)
+
+enum ff_result mrpc_char_array_serialize(const struct mrpc_char_array *char_array, struct ff_stream *stream)
+{
+	int i;
+	int len;
+	const wchar_t *value;
+	enum ff_result result = FF_FAILURE;
+
+	len = mrpc_char_array_get_len(char_array);
+	ff_assert(len >= 0);
+	if (len > MAX_CHAR_ARRAY_SIZE)
+	{
+		goto end;
+	}
+
+	result = mrpc_uint32_serialize((uint32_t) len, stream);
+	if (result != FF_SUCCESS)
+	{
+		goto end;
+	}
+
+	value = mrpc_char_array_get_value(char_array);
+	result = ff_stream_write(stream, value, len);
+
+end:
+	return result;
+}
+
+enum ff_result mrpc_char_array_unserialize(struct mrpc_char_array **char_array, struct ff_stream *stream)
+{
+	uint32_t i;
+	uint32_t u_len;
+	wchar_t *value;
+	enum ff_result result;
+
+	result = mrpc_uint32_unserialize(&u_len, stream);
+	if (result != FF_SUCCESS)
+	{
+		goto end;
+	}
+	if (u_len > MAX_CHAR_ARRAY_SIZE)
+	{
+		result = FF_FAILURE;
+		goto end;
+	}
+
+	value = (wchar_t *) ff_calloc(u_len, sizeof(value[0]));
+	result = ff_stream_read(stream, value, (int) u_len);
+	if (result != FF_SUCCESS)
+	{
+		ff_free(value);
+		goto end;
+	}
+
+	*char_array = mrpc_char_array_create(value, (int) u_len);
+
+end:
+	return result;
+}
