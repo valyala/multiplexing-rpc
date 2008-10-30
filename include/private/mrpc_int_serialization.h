1bb3b3a072d18cc9c05c9e98b13c92dacb584887
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-10-30 14:10:01 +0000

    mrpc_int_serialization refactored

--- include/private/mrpc_int_serialization.h
+++ include/private/mrpc_int_serialization.h
@@ -9,18 +9,6 @@ extern "C" {
 #endif
 
 /**
- * Serializes the 32bit unsigned integer into the stream.
- * Returns FF_SUCCESS on success, FF_FAILURE on error.
- */
-enum ff_result mrpc_uint32_serialize(uint32_t data, struct ff_stream *stream);
-
-/**
- * Unserializes the 32bit unsigned integer from the stream.
- * Returns FF_SUCCESS on success, FF_FAILURE on error.
- */
-enum ff_result mrpc_uint32_unserialize(uint32_t *data, struct ff_stream *stream);
-
-/**
  * Serializes the 64bit unsigned integer into the stream.
  * Returns FF_SUCCESS on success, FF_FAILURE on error.
  */
@@ -33,18 +21,6 @@ enum ff_result mrpc_uint64_serialize(uint64_t data, struct ff_stream *stream);
 enum ff_result mrpc_uint64_unserialize(uint64_t *data, struct ff_stream *stream);
 
 /**
- * Serializes 32bit signed integer into the stream.
- * Returns FF_SUCCESS on success, FF_FAILURE on error.
- */
-enum ff_result mrpc_int32_serialize(int32_t data, struct ff_stream *stream);
-
-/**
- * Unserializes 32bit signed integer from the stream.
- * Returns FF_SUCCESS on success, FF_FAILURE on error.
- */
-enum ff_result mrpc_int32_unserialize(int32_t *data, struct ff_stream *stream);
-
-/**
  * Serializes 64bit signed integer into the stream.
  * Returns FF_SUCCESS on success, FF_FAILURE on error.
  */
@@ -55,6 +31,30 @@ enum ff_result mrpc_int64_serialize(int64_t data, struct ff_stream *stream);
  * Returns FF_SUCCESS on success, FF_FAILURE on error.
  */
 enum ff_result mrpc_int64_unserialize(int64_t *data, struct ff_stream *stream);
+
+/**
+ * Serializes the 32bit unsigned integer into the stream.
+ * Returns FF_SUCCESS on success, FF_FAILURE on error.
+ */
+enum ff_result mrpc_uint32_serialize(uint32_t data, struct ff_stream *stream);
+
+/**
+ * Unserializes the 32bit unsigned integer from the stream.
+ * Returns FF_SUCCESS on success, FF_FAILURE on error.
+ */
+enum ff_result mrpc_uint32_unserialize(uint32_t *data, struct ff_stream *stream);
+
+/**
+ * Serializes 32bit signed integer into the stream.
+ * Returns FF_SUCCESS on success, FF_FAILURE on error.
+ */
+enum ff_result mrpc_int32_serialize(int32_t data, struct ff_stream *stream);
+
+/**
+ * Unserializes 32bit signed integer from the stream.
+ * Returns FF_SUCCESS on success, FF_FAILURE on error.
+ */
+enum ff_result mrpc_int32_unserialize(int32_t *data, struct ff_stream *stream);
 
 #ifdef __cplusplus
 }
--- src/mrpc_int_serialization.c
+++ src/mrpc_int_serialization.c
@@ -5,65 +5,11 @@
 
 #define BITS_PER_OCTET 7
 #define MAX_UINT_N_OCTETS(n) (((n) + BITS_PER_OCTET - 1) / BITS_PER_OCTET)
-#define MAX_UINT32_OCTETS MAX_UINT_N_OCTETS(32)
 #define MAX_UINT64_OCTETS MAX_UINT_N_OCTETS(64)
 #define OCTET_MASK ((1 << BITS_PER_OCTET) - 1)
 #define CONTINUE_NUMBER_FLAG (1 << BITS_PER_OCTET)
 
-enum ff_result mrpc_uint32_serialize(uint32_t data, struct ff_stream *stream)
-{
-	int len = 0;
-	uint8_t buf[MAX_UINT32_OCTETS];
-	enum ff_result result;
-
-	do
-	{
-		uint8_t octet;
-
-		ff_assert(len < MAX_UINT32_OCTETS);
-		octet = (uint8_t) (data & OCTET_MASK);
-		data >>= BITS_PER_OCTET;
-		octet |= (uint8_t) ((data != 0) ? CONTINUE_NUMBER_FLAG : 0);
-		buf[len] = octet;
-		len++;
-	}
-	while (data != 0);
-
-	result = ff_stream_write(stream, buf, len);
-	return result;
-}
-
-enum ff_result mrpc_uint32_unserialize(uint32_t *data, struct ff_stream *stream)
-{
-	int len = 0;
-	uint32_t u_data = 0;
-	uint8_t octet;
-	enum ff_result result = FF_FAILURE;
-
-	do
-	{
-		if (len >= MAX_UINT32_OCTETS)
-		{
-			result = FF_FAILURE;
-			goto end;
-		}
-		result = ff_stream_read(stream, &octet, 1);
-		if (result != FF_SUCCESS)
-		{
-			goto end;
-		}
-		len++;
-		u_data <<= BITS_PER_OCTET;
-		u_data |= (octet & OCTET_MASK);
-	}
-	while ((octet & CONTINUE_NUMBER_FLAG) != 0);
-
-	*data = u_data;
-	result = FF_SUCCESS;
-
-end:
-	return result;
-}
+#define MAX_UINT32_VALUE (~0ull)
 
 enum ff_result mrpc_uint64_serialize(uint64_t data, struct ff_stream *stream)
 {
@@ -121,33 +67,6 @@ end:
 	return result;
 }
 
-enum ff_result mrpc_int32_serialize(int32_t data, struct ff_stream *stream)
-{
-	uint32_t u_data;
-	enum ff_result result;
-
-	u_data = (data << 1) ^ (data >> 31);
-	result = uint32_serialize(u_data, stream);
-
-	return result;
-}
-
-enum ff_result mrpc_int32_unserialize(int32_t *data, struct ff_stream *stream)
-{
-	enum ff_result result;
-
-	result = uint32_unserialize(&u_data, stream);
-	if (result == FF_SUCCESS)
-	{
-		uint32_t u_data;
-
-		u_data = (u_data >> 1) ^ (-(u_data & 0x01));
-		*data = (int32_t) u_data;
-	}
-
-	return result;
-}
-
 enum ff_result mrpc_int64_serialize(int64_t data, struct ff_stream *stream)
 {
 	uint64_t u_data;
@@ -170,6 +89,61 @@ enum ff_result mrpc_int64_unserialize(int64_t *data, struct ff_stream *stream)
 
 		u_data = (u_data >> 1) ^ (-(u_data & 0x01));
 		*data = (int64_t) u_data;
+	}
+
+	return result;
+}
+
+enum ff_result mrpc_uint32_serialize(uint32_t data, struct ff_stream *stream)
+{
+	enum ff_result result;
+
+	result = mrpc_uint64_serialize((uint64_t) data, stream);
+	return result;
+}
+
+enum ff_result mrpc_uint32_unserialize(uint32_t *data, struct ff_stream *stream)
+{
+	enum ff_result result;
+	uint64_t u64_data;
+
+	result = mrpc_uint64_unserialize(&u64_data, stream);
+	if (result == FF_SUCCESS)
+	{
+		if (u_data < MAX_UINT32_VALUE)
+		{
+			*data = (uint32_t) u_data;
+		}
+		else
+		{
+			result = FF_FAILURE;
+		}
+	}
+	return result;
+}
+
+enum ff_result mrpc_int32_serialize(int32_t data, struct ff_stream *stream)
+{
+	uint32_t u_data;
+	enum ff_result result;
+
+	u_data = (data << 1) ^ (data >> 31);
+	result = uint32_serialize(u_data, stream);
+
+	return result;
+}
+
+enum ff_result mrpc_int32_unserialize(int32_t *data, struct ff_stream *stream)
+{
+	enum ff_result result;
+
+	result = uint32_unserialize(&u_data, stream);
+	if (result == FF_SUCCESS)
+	{
+		uint32_t u_data;
+
+		u_data = (u_data >> 1) ^ (-(u_data & 0x01));
+		*data = (int32_t) u_data;
 	}
 
 	return result;
