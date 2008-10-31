0e3bae251be302a3128e9d57058c37d9084becd0
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-10-31 12:02:30 +0000

    added comments to the mrpc_bitmap.h

--- include/private/mrpc_bitmap.h
+++ include/private/mrpc_bitmap.h
@@ -9,12 +9,26 @@ extern "C" {
 
 struct mrpc_bitmap;
 
+/**
+ * creates a bitmap
+ */
 struct mrpc_bitmap *mrpc_bitmap_create(int size);
 
+/**
+ * deletes the bitmap
+ */
 void mrpc_bitmap_delete(struct mrpc_bitmap *bitmap);
 
+/**
+ * searches for zero bit in the bitmap, sets it to 1 and returns its index.
+ * Returns -1 if there are no zero bits in the bitmap.
+ */
 int mrpc_bitmap_acquire_bit(struct mrpc_bitmap *bitmap);
 
+/**
+ * Clears the bit number n in the bitmap from 1 to 0.
+ * This bit must be previously acquired by mrpc_bitmap_acquire_bit() function.
+ */
 void mrpc_bitmap_release_bit(struct mrpc_bitmap *bitmap, int n);
 
 #ifdef __cplusplus
