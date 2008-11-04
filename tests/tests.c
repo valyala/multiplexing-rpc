d8ac7fcc7e88b97c5ae7c5849d54d438d9f3d803
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-11-04 11:55:44 +0000

    added tests for [w]char_array

---
+++ tests/tests.c
@@ -0,0 +1,155 @@
+#include "ff/ff_core.h"
+#include "mrpc/mrpc_char_array.h"
+#include "mrpc/mrpc_wchar_array.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#ifdef NDEBUG
+	#undef NDEBUG
+	#include <assert.h>
+	#define NDEBUG
+#else
+	#include <assert.h>
+#endif
+
+#define LOG_FILENAME L"mrpc_tests_log.txt"
+
+#define ASSERT(expr, msg) assert((expr) && (msg))
+
+#pragma region mrpc_char_array tests
+
+static void test_char_array_create_delete()
+{
+	struct mrpc_char_array *char_array;
+	char *s1;
+	const char *s2;
+	int len;
+
+	s1 = (char *) ff_calloc(4, sizeof(s1[0]));
+	memcpy(s1, "foo", 3 * sizeof(s1[0]));
+	char_array = mrpc_char_array_create(s1, 3);
+	ASSERT(char_array != NULL, "mrpc_char_array_create() cannot return NULL");
+	len = mrpc_char_array_get_len(char_array);
+	ASSERT(len == 3, "unexpected length of the char array received");
+	s2 = mrpc_char_array_get_value(char_array);
+	ASSERT(s1 == s2, "unexpected value received from the char_array");
+	mrpc_char_array_dec_ref(char_array);
+	/* mrpc_charr_array_dec_ref() must delete the memory allocated for the s1 */
+}
+
+static void test_char_array_basic()
+{
+	struct mrpc_char_array *char_array;
+	char *s;
+	uint32_t hash_value;
+	int i;
+
+	s = (char *) ff_calloc(6, sizeof(s[0]));
+	memcpy(s, "abcde", 5 * sizeof(s[0]));
+	char_array = mrpc_char_array_create(s, 5);
+
+	for (i = 0; i < 10; i++)
+	{
+		mrpc_char_array_inc_ref(char_array);
+	}
+	mrpc_char_array_dec_ref(char_array);
+	mrpc_char_array_inc_ref(char_array);
+	for (i = 0; i < 10; i++)
+	{
+		mrpc_char_array_dec_ref(char_array);
+	}
+
+	hash_value = mrpc_char_array_get_hash(char_array, 123);
+
+	mrpc_char_array_dec_ref(char_array);
+}
+
+static void test_char_array()
+{
+	ff_core_initialize(LOG_FILENAME);
+
+	test_char_array_create_delete();
+	test_char_array_basic();
+
+	ff_core_shutdown();
+}
+
+/* end of mrpc_char_array tests */
+#pragma endregion
+
+
+#pragma region mrpc_wchar_array tests
+
+static void test_wchar_array_create_delete()
+{
+	struct mrpc_wchar_array *wchar_array;
+	wchar_t *s1;
+	const wchar_t *s2;
+	int len;
+
+	s1 = (wchar_t *) ff_calloc(4, sizeof(s1[0]));
+	memcpy(s1, L"foo", 3 * sizeof(s1[0]));
+	wchar_array = mrpc_wchar_array_create(s1, 3);
+	ASSERT(wchar_array != NULL, "mrpc_wchar_array_create() cannot return NULL");
+	len = mrpc_wchar_array_get_len(wchar_array);
+	ASSERT(len == 3, "unexpected length of the wchar array received");
+	s2 = mrpc_wchar_array_get_value(wchar_array);
+	ASSERT(s1 == s2, "unexpected value received from the wchar_array");
+	mrpc_wchar_array_dec_ref(wchar_array);
+	/* mrpc_wcharr_array_dec_ref() must delete the memory allocated for the s1 */
+}
+
+static void test_wchar_array_basic()
+{
+	struct mrpc_wchar_array *wchar_array;
+	wchar_t *s;
+	uint32_t hash_value;
+	int i;
+
+	s = (wchar_t *) ff_calloc(6, sizeof(s[0]));
+	memcpy(s, L"abcde", 5 * sizeof(s[0]));
+	wchar_array = mrpc_wchar_array_create(s, 5);
+
+	for (i = 0; i < 10; i++)
+	{
+		mrpc_wchar_array_inc_ref(wchar_array);
+	}
+	mrpc_wchar_array_dec_ref(wchar_array);
+	mrpc_wchar_array_inc_ref(wchar_array);
+	for (i = 0; i < 10; i++)
+	{
+		mrpc_wchar_array_dec_ref(wchar_array);
+	}
+
+	hash_value = mrpc_wchar_array_get_hash(wchar_array, 123);
+
+	mrpc_wchar_array_dec_ref(wchar_array);
+}
+
+static void test_wchar_array()
+{
+	ff_core_initialize(LOG_FILENAME);
+
+	test_wchar_array_create_delete();
+	test_wchar_array_basic();
+
+	ff_core_shutdown();
+}
+
+/* end of mrpc_char_array tests */
+#pragma endregion
+
+static void test_all()
+{
+	test_char_array();
+	test_wchar_array();
+}
+
+int main(int argc, char* argv[])
+{
+	test_all();
+	printf("ALL TESTS PASSED\n");
+
+	return 0;
+}
