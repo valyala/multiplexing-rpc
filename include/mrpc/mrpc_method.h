5d578e1afa1e98ddb1fc17d7d9315dda57f71c69
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-11-05 18:00:05 +0000

    added comment for the mrpc_method_constructor callback

--- include/mrpc/mrpc_method.h
+++ include/mrpc/mrpc_method.h
@@ -9,6 +9,12 @@ extern "C" {
 
 struct mrpc_method;
 
+/**
+ * This callback must create mrpc_method object using the mrpc_method_create_server_method()
+ * or mrpc_method_create_client_method() functions.
+ * It must always return correct result.
+ * Array of such a callbacks must be passed to the mrpc_interface_create().
+ */
 typedef struct mrpc_method *(*mrpc_method_constructor)();
 
 #ifdef __cplusplus
