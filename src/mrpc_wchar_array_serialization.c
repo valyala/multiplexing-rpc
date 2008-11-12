b1a7d62481252ce9b17819e8875ae82c079f4d52
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-11-12 15:33:58 +0000

    added ff_log_debug() to the mrpc_wchar_array_serialization.c

--- src/mrpc_wchar_array_serialization.c
+++ src/mrpc_wchar_array_serialization.c
@@ -25,12 +25,14 @@ enum ff_result mrpc_wchar_array_serialize(struct mrpc_wchar_array *wchar_array, 
 	ff_assert(len >= 0);
 	if (len > MAX_WCHAR_ARRAY_SIZE)
 	{
+		ff_log_warning(L"wchar_array=%p has too large len=%d for serialization. It must be less than or eqal to %d", wchar_array, len, MAX_WCHAR_ARRAY_SIZE);
 		goto end;
 	}
 
 	result = mrpc_uint32_serialize((uint32_t) len, stream);
 	if (result != FF_SUCCESS)
 	{
+		ff_log_debug(L"cannot serialize wchar_array=%p length len=%d into the stream=%p. See previous messages for more info", wchar_array, len, stream);
 		goto end;
 	}
 
@@ -44,6 +46,7 @@ enum ff_result mrpc_wchar_array_serialize(struct mrpc_wchar_array *wchar_array, 
 		result = mrpc_uint32_serialize(ch, stream);
 		if (result != FF_SUCCESS)
 		{
+			ff_log_debug(L"cannot serialize character ch=%lu number %d from the wchar_array=%p into the stream=%p. See previous messages for more info", ch, i, wchar_array, stream);
 			goto end;
 		}
 	}
@@ -63,10 +66,12 @@ enum ff_result mrpc_wchar_array_unserialize(struct mrpc_wchar_array **wchar_arra
 	result = mrpc_uint32_unserialize(&u_len, stream);
 	if (result != FF_SUCCESS)
 	{
+		ff_log_debug(L"cannot unserialize wchar_array length from the stream=%p. See previous messages for more info", stream);
 		goto end;
 	}
 	if (u_len > MAX_WCHAR_ARRAY_SIZE)
 	{
+		ff_log_debug(L"unserialized from the stream=%p length len=%lu of the wchar_array must be less than or equal to %d", stream, u_len, MAX_WCHAR_ARRAY_SIZE);
 		result = FF_FAILURE;
 		goto end;
 	}
@@ -79,6 +84,7 @@ enum ff_result mrpc_wchar_array_unserialize(struct mrpc_wchar_array **wchar_arra
 		result = mrpc_uint32_unserialize(&ch, stream);
 		if (result != FF_SUCCESS)
 		{
+			ff_log_debug(L"cannot unserialize charachter number %d from the stream=%p for wchar_array. See previous messages for more info", i, stream);
 			ff_free(value);
 			goto end;
 		}
@@ -88,6 +94,7 @@ enum ff_result mrpc_wchar_array_unserialize(struct mrpc_wchar_array **wchar_arra
 			 * should be treated as invalid, because they cannot be represented
 			 * on architectures where sizeof(whcar_t) == 2.
 			 */
+			ff_log_debug(L"wrong character ch=%lu has been read from the stream=%p. It's value must be less than or equal to %d", ch, stream, 0x10000);
 			ff_free(value);
 			result = FF_FAILURE;
 			goto end;
