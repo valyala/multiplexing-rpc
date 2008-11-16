1f9533e16ee18d06714f9648384fcdea5c8bd5e5
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-11-16 20:06:38 +0000

    mrpc_server_request_processor refactored

--- src/mrpc_server_request_processor.c
+++ src/mrpc_server_request_processor.c
@@ -25,27 +25,36 @@ struct mrpc_server_request_processor
 static void process_request_func(void *ctx)
 {
 	struct mrpc_server_request_processor *request_processor;
+	struct mrpc_packet_stream *packet_stream;
+	struct mrpc_interface *service_interface;
+	void *service_ctx;
+	struct ff_stream *stream;
+	uint8_t request_id;
 	enum ff_result result;
 
 	request_processor = (struct mrpc_server_request_processor *) ctx;
-
 	ff_assert(request_processor->service_interface != NULL);
 
-	mrpc_packet_stream_initialize(request_processor->packet_stream, request_processor->request_id);
-	result = mrpc_data_process_remote_call(request_processor->service_interface, request_processor->service_ctx, request_processor->stream);
+	packet_stream = request_processor->packet_stream;
+	request_id = request_processor->request_id;
+	service_interface = request_processor->service_interface;
+	service_ctx = request_processor->service_ctx;
+	stream = request_processor->stream;
+
+	mrpc_packet_stream_initialize(packet_stream, request_id);
+	result = mrpc_data_process_remote_call(service_interface, service_ctx, stream);
 	if (result != FF_SUCCESS)
 	{
-		ff_log_debug(L"cannot process remote call from the stream=%p using the service_interface=%p, service_ctx=%p",
-			request_processor->stream, request_processor->service_interface, request_processor->service_ctx);
+		ff_log_debug(L"cannot process remote call from the stream=%p using the service_interface=%p, service_ctx=%p", stream, service_interface, service_ctx);
 	}
-	mrpc_packet_stream_shutdown(request_processor->packet_stream);
+	mrpc_packet_stream_shutdown(packet_stream);
 	if (result != FF_SUCCESS)
 	{
 		request_processor->notify_error_func(request_processor->notify_error_func_ctx);
 	}
 	request_processor->service_interface = NULL;
 	request_processor->service_ctx = NULL;
-	request_processor->release_func(request_processor->release_func_ctx, request_processor, request_processor->request_id);
+	request_processor->release_func(request_processor->release_func_ctx, request_processor, request_id);
 }
 
 struct mrpc_server_request_processor *mrpc_server_request_processor_create(mrpc_server_request_processor_release_func release_func, void *release_func_ctx,
