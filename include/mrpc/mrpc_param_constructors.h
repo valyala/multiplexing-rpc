fa555fd3c8ecf56b6156d66da14f5bf5b2ac04e9
valyala <valyala@f0df441e-8fd6-11dd-bc21-9b04cd9ec0f8>
2008-11-17 13:45:21 +0000

    added mrpc_param_constructors.h, which includes all available param constructors in the library

---
+++ include/mrpc/mrpc_param_constructors.h
@@ -0,0 +1,9 @@
+#ifndef MRPC_PARAM_CONSTRUCTORS_PUBLIC
+#define MRPC_PARAM_CONSTRUCTORS_PUBLIC
+
+#include "mrpc/mrpc_char_array_param.h"
+#include "mrpc/mrpc_wchar_array_param.h"
+#include "mrpc/mrpc_blob_param.h"
+#include "mrpc/mrpc_int_param.h"
+
+#endif
